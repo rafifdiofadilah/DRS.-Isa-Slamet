3eb25ecd2fe44516c04bb7f6e6b913cf74e8618d
rafif dio fadilah <162969608+rafifdiofadilah@users.noreply.github.com>
2024-11-27 08:12:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Menunggu beberapa detik dan mengarahkan pengunjung ke halaman utama
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = 'home.html'; // Ganti dengan URL halaman utama Anda
+    }, 5000); // 5000ms = 5 detik
+};
